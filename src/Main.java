0d6814cea7fc5c29892f8879c4f940fbb70ec8a0
Diesel127 <retina127127@gmail.com>
2025-08-08 10:40:43 +0200

    lesson1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        String string1 = "Амиго";
+        String string2 = string1;
+        String string3 = new String(string1);
+        String same = "ссылки на строки одинаковые";
+        String different = "ссылки на строки разные";
+        if (string1 == string2){
+            System.out.println(same);
+        } else {
+            System.out.println(different);
+        } if (string2 == string3){
+            System.out.println(same);
+        } else {
+            System.out.println(different);
+        } if (string1 == string3){
+            System.out.println(same);
+        } else {
+            System.out.println(different);
         }
     }
 }
