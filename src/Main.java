29acb8640064571b1bee7e90cab88b08ebf2e687
Diesel127 <retina127127@gmail.com>
2025-08-14 17:18:44 +0200

    lesson9

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,19 @@ public class Main {
     public static void main(String[] args) {
 
         int n = 0;
-        while (n < 5) {
+        while (n < 10){
             int m = 0;
-            while (m < 10) {
-                System.out.print("Q");
-                m++;
+            while (m < 20){
+                if (n == 0 || n == 9){
+                    System.out.print("Б");
+                    m++;
+                } else if (m == 0 || m == 19){
+                    System.out.print("Б");
+                    m++;
+                } else {
+                    System.out.print(" ");
+                    m++;
+                }
             }
             System.out.println();
             n++;
