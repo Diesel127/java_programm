f38b09a577d302ee9b7967d4d2f0fe520111de9f
Diesel127 <retina127127@gmail.com>
2025-08-26 15:12:36 +0200

    lesson3

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,20 @@
-import java.util.Scanner;
+
 
 public class Main {
 
-    static Scanner scanner = new Scanner(System.in);
-
     public static void main(String[] args) {
-        System.out.println("Робот-помощник v2.0.");
-        // вызовите ниже методы в правильном порядке
-        sayHello();
-        sayHelloByTime();
-        welcomeUserByName();
-        printSuccess();
+        double[] expenses = {1772.5, 367.0, 120.6, 2150.2, 874.0, 1.0, 1459.4};
+        double maxExpense = findMaxExpense(expenses); // вызовите метод и присвойте maxExpense значение его результата
+        System.out.println("Самая большая трата недели " + maxExpense);
     }
 
-    public static void welcomeUserByName() {
-        System.out.println("Как вас зовут?");
-        String name = scanner.next();
-        printCity();
-        String city = scanner.next(); // допишите чтение города
-        System.out.println("Рад познакомиться, " + name + " из " + city + "!");
-    }
-
-    public static void printSuccess() {
-        System.out.println("У вас уже неплохо получается программировать!");
-    }
-
-    public static void sayHello(){
-        System.out.println("Привет!");
-    } // допишите метод sayHello(), который печатает: Привет!
-
-    public static void printCity(){
-        System.out.println("Из какого вы города?");
-    } // допишите метод printCity(), который печатает: Из какого вы города?
-
-    public static void sayHelloByTime() {
-        System.out.println("Который час?");
-        int currentHour = scanner.nextInt();
-        if (currentHour < 6) {
-            System.out.println("Доброй ночи!");
-        } else if (currentHour > 22) {
-            System.out.println("Доброй ночи!");
-        } else if (currentHour < 12) {
-            System.out.println("Доброе утро!");
-        } else if (currentHour < 18) {
-            System.out.println("Добрый день!");
-        } else {
-            System.out.println("Добрый вечер!");
+    public static double findMaxExpense(double[] expenses) { // объявите метод findMaxExpense
+        double maxExpense = expenses[0];
+        for (int i = 0; i < expenses.length; i++) {
+            if (expenses[i] > maxExpense) {
+                maxExpense = expenses[i];
+            }
         }
+        return maxExpense;
     }
 }
