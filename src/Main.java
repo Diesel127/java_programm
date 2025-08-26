e2416469fc23ecc3caf2fe1a724192df930798be
Diesel127 <retina127127@gmail.com>
2025-08-26 13:52:59 +0200

    lesson2

--- src/Main.java
+++ src/Main.java
@@ -5,30 +5,47 @@ public class Main {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+        System.out.println("Робот-помощник v2.0.");
+        // вызовите ниже методы в правильном порядке
+        sayHello();
+        sayHelloByTime();
+        welcomeUserByName();
+        printSuccess();
     }
 
-        static void welcomeUserByName () { // объявите метод welcomeUserByName
-            System.out.println("Как вас зовут?");
-            String name = scanner.nextLine(); // сохраните введённое пользователем имя в переменную name
-            System.out.println("Рад познакомиться, " + name + "!");
-        }
+    public static void welcomeUserByName() {
+        System.out.println("Как вас зовут?");
+        String name = scanner.next();
+        printCity();
+        String city = scanner.next(); // допишите чтение города
+        System.out.println("Рад познакомиться, " + name + " из " + city + "!");
+    }
 
-        public static void sayHelloByTime () {// объявите метод sayHelloByTime
-            System.out.println("Который час?");
-            int currentHour = scanner.nextInt();// спросите у пользователя "Который час?" и сохраните ответ в переменную currentHour
-            if (currentHour > 22 || currentHour < 6) {
-                System.out.println("Доброй ночи!");
-            } else if (currentHour > 6 && currentHour < 12) {
-                System.out.println("Доброе утро!"); // в зависимости от времени предусмотрите печать приветствий
-            } else if (currentHour > 12 && currentHour < 18) {
-                System.out.println("Добрый день!");
-            } else if (currentHour > 18 && currentHour < 22) {
-                System.out.println("Добрый вечер!");
-            }
-        }
+    public static void printSuccess() {
+        System.out.println("У вас уже неплохо получается программировать!");
+    }
 
-        public static void printSuccess () {
-            System.out.println("У вас уже неплохо получается программировать!");
-        } // объявите метод printSuccess
-        // метод printSuccess должен печатать строку "У вас уже неплохо получается программировать!"
+    public static void sayHello(){
+        System.out.println("Привет!");
+    } // допишите метод sayHello(), который печатает: Привет!
+
+    public static void printCity(){
+        System.out.println("Из какого вы города?");
+    } // допишите метод printCity(), который печатает: Из какого вы города?
+
+    public static void sayHelloByTime() {
+        System.out.println("Который час?");
+        int currentHour = scanner.nextInt();
+        if (currentHour < 6) {
+            System.out.println("Доброй ночи!");
+        } else if (currentHour > 22) {
+            System.out.println("Доброй ночи!");
+        } else if (currentHour < 12) {
+            System.out.println("Доброе утро!");
+        } else if (currentHour < 18) {
+            System.out.println("Добрый день!");
+        } else {
+            System.out.println("Добрый вечер!");
+        }
+    }
 }
